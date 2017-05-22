d1907e63a160c8e16a47da941ea51b80396c35a2
Julio Cesar da Silva <s2it_jsilva@uolinc.com>
2017-05-22 09:37:38 -0300

    Ajustes nas tasks de automacao

--- gulpfile.js
+++ gulpfile.js
@@ -5,56 +5,131 @@ var uglify      = require("gulp-uglify");
 var watch       = require("gulp-watch");
 var jshint      = require("gulp-jshint");
 var concat      = require("gulp-concat");
-var cssmin = require('gulp-cssmin');
-
+var cssmin      = require('gulp-cssmin');
+var concatCss   = require('gulp-concat-css');
+var clean       = require('gulp-clean');
 
 
 gulp.task("default", function () {
-    gutil.log("Gulp is working!");
-    gulp.start('watch');
-});
-
-
-
-gulp.task('jshint', function () {
-    gutil.log('Starting jshint task.');
-    return gulp.src([
-        './app/components/**/**/**/*.js', 
-        './app/components/*.js'
-    ])
-    .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish'));
-});
-gulp.task('uglify', function () {
-    gutil.log('Starting uglify task.');
-    return gulp.src(['./app/components/**/**/**/*.js'])
-    .pipe(uglify())
-    .pipe(gulp.dest('./app/dist'));
-});
-gulp.task('minify', function() {
-    gutil.log('Starting minify task.');
-    return gulp.src([
-        './app/index.html', 
-        './app/components/**/**/**/*.html'
-    ])
-    .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest('./app/dist'));
-});
-gulp.task('minify-css', function () {
-    gutil.log('Starting minify-css task.');
-    return gulp.src(['./app/components/**/**/**/*.css'])
-    .pipe(cssmin())
-    .pipe(gulp.dest('./app/dist'));
+    gulp.start('uglify');
+    gulp.start('html-min');
+    gulp.start('css-min');
+    gulp.start('concat-js');
+    gulp.start('concat-css');
+    gulp.start('remove');
 });
 
 
 gulp.task('watch', function () {
-    gutil.log('Starting watch task.');
     gulp.watch('./app/components/**/**/**/*.js', function (event) {
         gutil.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
         gulp.start('jshint');
         gulp.start('uglify');
-        gulp.start('minify');
-        gulp.start('minify-css');
+        gulp.start('html-min');
+        gulp.start('css-min');
+        gulp.start('concat-js');
+        gulp.start('concat-css');
     });
  });
+
+
+/* HINT */
+gulp.task('jshint', function () {
+    return gulp.src([
+        './app/modules.js',
+        './app/components/directives/directives.js',
+        './app/components/directives/popover/tooltip.js',
+        './app/components/directives/popover/popover.js',
+        './app/components/directives/popover/helpers/*.js',
+        './app/components/directives/alert/*.js',
+        './app/components/filters/filters.js',
+        './app/components/filters/moment/moment.js',
+        './app/components/filters/treusted/treusted.js',
+        './app/components/services/services.js',
+        './app/components/services/contact/contactService.js',
+        './app/components/services/notification/notification.js',
+        './app/app.js',
+        './app/contacts/contacts.js',
+        './app/info/info.js'
+    ])
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+
+/* JS minifier */
+gulp.task('uglify', function () {
+    return gulp.src([
+        './app/modules.js',
+        './app/components/directives/directives.js',
+        './app/components/directives/popover/tooltip.js',
+        './app/components/directives/popover/popover.js',
+        './app/components/directives/popover/helpers/*.js',
+        './app/components/directives/alert/*.js',
+        './app/components/filters/filters.js',
+        './app/components/filters/moment/moment.js',
+        './app/components/filters/treusted/treusted.js',
+        './app/components/services/services.js',
+        './app/components/services/contact/contactService.js',
+        './app/components/services/notification/notification.js',
+        './app/app.js',
+        './app/contacts/contacts.js',
+        './app/info/info.js'
+    ])
+    .pipe(uglify())
+    .pipe(gulp.dest('./app/dist/temp'));
+});
+
+
+/* HTML minifier */
+gulp.task('html-min', function() {
+    return gulp.src([
+        './app/index.html', 
+        './app/components/directives/alert/alert.html',
+        './app/components/directives/popover/popover.html',
+        './app/components/directives/popover/tooltip.html',
+        './app/contacts/contacts.html',
+        './app/info/info.html'
+    ])
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('./app/dist'));
+});
+
+
+/* CSS minifier */
+gulp.task('css-min', function () {
+    return gulp.src([
+        './app/components/angular-motion/modules/fade.css',
+        './app/components/angular-motion/angular-motion.css',
+        './app/components/directives/alert/alert.css',
+        './app/app.css',
+        './app/custom.css'
+    ])
+    .pipe(cssmin())
+    .pipe(gulp.dest('./app/dist/temp'));
+});
+
+
+/* Concat */
+gulp.task('concat-js', ['uglify'], function() {
+    return gulp.src([
+        './app/dist/temp/*.js'
+    ])
+    .pipe(concat('bundle.js'))
+    .pipe(gulp.dest('./app/dist'));
+});
+
+gulp.task('concat-css',  ['css-min'], function () {
+  return gulp.src([
+        './app/dist/temp/*.css'
+    ])
+    .pipe(concatCss('bundle.css'))
+    .pipe(gulp.dest('./app/dist'));
+});
+
+
+/* CLEAN */
+gulp.task('remove', ['concat-js', 'concat-css'], function () {
+    gutil.log('Starting remove task.');
+    return gulp.src('./app/dist/temp', {read: false}).pipe(clean());
+});
